076344dc63f36e26e8568d312acfc597005a666a
d.espitia15 <tuemail@ejemplo.com>
2025-04-29 12:39:27 -0500

    first design movements form

--- src/app/add/components/movements-form/movements-form.component.ts
+++ src/app/add/components/movements-form/movements-form.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonContent, IonItem, IonLabel, IonInput, IonTextarea, IonDatetime, IonButton, IonSelect, IonSelectOption, IonTitle } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-movements-form',
   templateUrl: './movements-form.component.html',
   styleUrls: ['./movements-form.component.scss'],
+  imports:[IonHeader, IonToolbar, IonContent, IonItem, IonLabel, IonInput, IonTextarea, IonDatetime, IonButton, IonSelect, IonSelectOption, IonTitle]
 })
 export class MovementsFormComponent  implements OnInit {
 
