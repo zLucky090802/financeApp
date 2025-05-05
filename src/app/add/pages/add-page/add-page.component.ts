34b5602b84202c0475d59071eb167321037bfe93
zLucky090802 <d.espitia15@gmail.com>
2025-05-05 12:26:59 -0500

    re design de add page

--- src/app/add/pages/add-page/add-page.component.ts
+++ src/app/add/pages/add-page/add-page.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonGrid, IonRow, IonContent, IonCol } from "@ionic/angular/standalone";
+import { IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonGrid, IonRow, IonContent, IonCol, IonIcon } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-add-page',
   templateUrl: './add-page.component.html',
   styleUrls: ['./add-page.component.scss'],
-  imports: [IonCol, IonCardContent, IonCardHeader, IonCardTitle, IonCard, IonGrid, IonRow, IonContent, RouterLink]
+  imports: [IonIcon, IonCol, IonCardContent, IonCardHeader, IonCardTitle, IonCard, IonGrid, IonRow, IonContent, RouterLink]
 })
 export class AddPageComponent  implements OnInit {
 
