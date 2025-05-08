447e867372e2c179d08aadb758d37ba4c3831617
zLucky090802 <d.espitia15@gmail.com>
2025-05-08 10:36:09 -0500

    card category make at add page

--- src/app/add/components/category-form/category-form.component.ts
+++ src/app/add/components/category-form/category-form.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IonicModule} from "@ionic/angular";
+
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.scss'],
+  imports:[IonicModule]
 })
 export class CategoryFormComponent  implements OnInit {
 
