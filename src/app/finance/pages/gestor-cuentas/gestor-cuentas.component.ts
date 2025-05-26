279859e05affb52237f68e4d65594b90e03a6f71
zLucky090802 <d.espitia15@gmail.com>
2025-05-26 14:15:03 -0500

    design of gestor cuentas pages

--- src/app/finance/pages/gestor-cuentas/gestor-cuentas.component.ts
+++ src/app/finance/pages/gestor-cuentas/gestor-cuentas.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-gestor-cuentas',
   templateUrl: './gestor-cuentas.component.html',
   styleUrls: ['./gestor-cuentas.component.scss'],
+  imports:[IonicModule]
 })
 export class GestorCuentasComponent  implements OnInit {
 
