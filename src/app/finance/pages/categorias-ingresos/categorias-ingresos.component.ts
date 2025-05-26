fb4b0adf88de4af265fb4af904df5cee309789c9
zLucky090802 <d.espitia15@gmail.com>
2025-05-26 14:32:14 -0500

    categorias ingresos design make it

--- src/app/finance/pages/categorias-ingresos/categorias-ingresos.component.ts
+++ src/app/finance/pages/categorias-ingresos/categorias-ingresos.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-categorias-ingresos',
   templateUrl: './categorias-ingresos.component.html',
   styleUrls: ['./categorias-ingresos.component.scss'],
+  imports: [IonicModule]
 })
 export class CategoriasIngresosComponent  implements OnInit {
 
