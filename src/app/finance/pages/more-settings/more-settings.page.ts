9581550735d05e0f467062c0866f3b93cb104d5b
zLucky090802 <d.espitia15@gmail.com>
2025-05-26 13:22:10 -0500

    first design of settings

--- src/app/finance/pages/more-settings/more-settings.page.ts
+++ src/app/finance/pages/more-settings/more-settings.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-more-settings',
   templateUrl: './more-settings.page.html',
   styleUrls: ['./more-settings.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [ CommonModule, FormsModule, IonicModule]
 })
 export class MoreSettingsPage implements OnInit {
 
