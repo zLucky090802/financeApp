bafe55cfc585c420694715c3b99315788722e10c
zLucky090802 <d.espitia15@gmail.com>
2025-05-26 14:38:27 -0500

    categorias gastos design make it

--- src/app/finance/pages/categorias-gastos/categorias-gastos.component.ts
+++ src/app/finance/pages/categorias-gastos/categorias-gastos.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-categorias-gastos',
   templateUrl: './categorias-gastos.component.html',
   styleUrls: ['./categorias-gastos.component.scss'],
+  imports:[IonicModule]
 })
 export class CategoriasGastosComponent  implements OnInit {
 
